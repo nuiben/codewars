9ec6de11f7f3f970985714b06e2d9c3809f6bae0
nuiben <bgporte@outlook.com>
2022-05-20 15:32:29 -0500

    05-20-22 Spinning Rings

---
+++ spinningRings.go
@@ -0,0 +1,20 @@
+package kata
+
+func SpinningRings(innerMax, outerMax int) int {
+
+	//both inner and outer start at 0 (move 0)
+	//inner decrements, so it is = innerMax on move 1
+	//outer increments, so it is = 1 on move 1
+
+	var moves = 1
+	var iVal = innerMax
+	var oVal = 1
+
+	for iVal != oVal {
+		moves++
+		iVal = (iVal + innerMax) % (innerMax + 1)
+		oVal = (oVal + 1) % (outerMax + 1)
+	}
+
+	return moves
+}

5973d21b4679cf861463303e628b3f36e878f264
Ben Porter <102399623+nuiben@users.noreply.github.com>
2022-05-28 10:49:37 -0500

    Update oddTriangle.go

--- oddTriangle.go
+++ oddTriangle.go
@@ -1,25 +1,27 @@
 //Given the triangle of consecutive odd numbers:
 //
-// 				   1
-// 				3     5
-// 			 7     9    11
-// 		 13    15    17    19
-//    21    23    25    27    29
+// 			1
+// 	   	     3     5
+// 	          7     9    11
+// 	      13    15    17    19
+//         21    23    25    27    29
 //
 // Calculate the sum of the numbers in the nth row of this
 // triangle (starting at index 1) e.g.: (Input --> Output)
 // 1 --> 1
 // 2 --> 3 + 5 = 8
-// 3 --> 7 + 9 = 27
+// 3 --> 7 + 9 + 11 = 27
 //
-// Optimization: When I wrote this originally, I solved it to sum
+// Optimization: When I wrote this originally, I misread the prompt and solved it to sum
 // all rows before the nth row as well as the nth row. Failing to recognize
-// that the sum of each row was n^3.
-//
-// func RowSumOddNumbers(n int) int {
-//     return n * n * n
-// }
+// that the sum of each row was n^3, I overcomplicated the solution as I refactored
+// to correct my mistake. This is the optimal solution:
 
+ func RowSumOddNumbers(n int) int {
+     return n * n * n
+ }
+
+// This solution is still good, though inefficient.
 package kata
 
 func RowSumOddNumbers(n int) int {

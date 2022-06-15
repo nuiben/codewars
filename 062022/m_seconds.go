045bd5771bcf77a8a69a77e9dc14082eb3d6b122
nuiben <bgporte@outlook.com>
2022-06-15 13:53:29 -0500

    06-15-22 Milliseconds

---
+++ 062022/m_seconds.go
@@ -0,0 +1,5 @@
+package kata
+
+func Past(h, m, s int) int {
+	return (1000 * s) + (60000 * m) + (3600000 * h)
+}
--- 062022/sumOfIntervals.go
+++ 062022/sumOfIntervals.go
@@ -1,37 +1,12 @@
 package kata
 
-import "fmt"
-
 func SumOfIntervals(intervals [][2]int) int {
-	fmt.Println("input: ", intervals)
-	fmt.Println("len: ", len(intervals))
-	sum := 0
-	next := 0
-	//   mrgIntervals := intervals
-
-	for k, i := range intervals {
-		if k+1 < len(intervals) {
-			next = k + 1
-		}
-		fmt.Println("next: ", next)
-		fmt.Println("k: ", k, "   i: ", i)
-		//     fmt.Println("intervals[next][0]: ",intervals[next][0])
-		//     fmt.Println("i[0]: ",i[0])
-		if k != next && intervals[next][0] <= i[1] {
-			if intervals[next][0] < i[0] {
-				sum += i[1] - i[0]
-				sum += intervals[next][1] - intervals[0][1]
-				sum -= intervals[next][1] - intervals[next][0]
-				fmt.Println("the next interval doesn't overlap the current, sum: ", sum)
-			} else {
-				sum += intervals[next][1] - i[0]
-				fmt.Println("overlapping count: ", sum)
-				break
-			}
-		} else {
-			sum += i[1] - i[0]
-			fmt.Println("normal count: ", sum)
+	dict := make(map[int]int)
+	for _, i := range intervals {
+		for j := i[0]; j < i[1]; j++ {
+			dict[j] += 1
 		}
 	}
-	return sum
+	return len(dict)
+
 }
